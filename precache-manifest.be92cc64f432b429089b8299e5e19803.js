181b6a305957d24652dcfef5b8de7f782ae944be
slavon1265 <slavon1265@gmail.com>
2020-08-07 00:13:08 +0300

    Updates

---
+++ precache-manifest.be92cc64f432b429089b8299e5e19803.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1900d5608646f68c6f22567d002e38be",
+    "url": "/Insta-Analog/index.html"
+  },
+  {
+    "revision": "254759fdec2a4bda2983",
+    "url": "/Insta-Analog/static/css/main.b7ea1ee1.chunk.css"
+  },
+  {
+    "revision": "98ff6b50e412a20790fb",
+    "url": "/Insta-Analog/static/js/2.bcfe681a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Insta-Analog/static/js/2.bcfe681a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "254759fdec2a4bda2983",
+    "url": "/Insta-Analog/static/js/main.3ce89321.chunk.js"
+  },
+  {
+    "revision": "fc5119c0fc8b962957ce",
+    "url": "/Insta-Analog/static/js/runtime-main.41d1dbb3.js"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "/Insta-Analog/static/media/error.3b8ad90a.png"
+  },
+  {
+    "revision": "9e8a2b3937952b3de7aca04cb0d74020",
+    "url": "/Insta-Analog/static/media/logo.9e8a2b39.svg"
+  }
+]);
